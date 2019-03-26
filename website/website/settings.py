48f185698ee093a2498ee8efacb2bf7b481c51c7
yaniv11386 <yaniv11386@gmail.com>
2019-03-26 23:30:09 +0200

    created simple models and registered them to mongo_explorer admin

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mongo_explorer.apps.MongoExplorerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
